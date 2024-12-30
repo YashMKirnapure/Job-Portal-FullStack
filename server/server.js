7e12de3306987fdf1e1d525c2450759947a8ad46
YashKirnapure77 <yashkirnapure777@gmail.com>
2024-12-30 10:34:01 +0530

    Working on Controllers , Models , Routes in Backend

--- server/server.js
+++ server/server.js
@@ -5,12 +5,17 @@ import 'dotenv/config';
 import connectDB from './config/db.js';
 import * as Sentry from "@sentry/node";
 import { clerkWebhooks } from './controllers/webhooks.js';
+import companyRoutes from "./routes/companyRoutes.js";
+import connectCloudinary from './config/cloudinary.js';
+import jobRoutes from "./routes/jobRoutes.js";
+import userRoutes from './routes/userRoutes.js';
 
 // Initialize Express
 const app = express()
 
 // Connect to DB
 await connectDB()
+await connectCloudinary()
 
 // Middleware
 app.use(cors());
@@ -23,9 +28,12 @@ app.get('/',(req,res)=>{
 
 app.get("/debug-sentry", function mainHandler(req, res) {
     throw new Error("My first Sentry error!");
-  });
-  
+});
+
 app.post('/webhooks',clerkWebhooks)
+app.use('/api/company',companyRoutes)
+app.use('/api/jobs',jobRoutes)
+app.use('/api/users',userRoutes)
 
 // Port
 const PORT = process.env.PORT || 5000;
