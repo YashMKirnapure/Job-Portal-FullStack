ca67d72b0f5c9f15686f487912cbea4d7ca0b716
YashKirnapure77 <yashkirnapure777@gmail.com>
2025-01-01 17:10:26 +0530

    Almost Done

--- client/src/pages/ManageJobs.jsx
+++ client/src/pages/ManageJobs.jsx
@@ -4,13 +4,14 @@ import moment from 'moment';
 import {useNavigate} from 'react-router-dom';
 import {AppContext} from '../context/AppContext';
 import axios from 'axios';
+import Loading from '../components/Loading';
 
 const ManageJobs = () => 
 {
 
   const navigate = useNavigate();
 
-  const [jobs,setJobs] = useState([]);
+  const [jobs,setJobs] = useState(false);
 
   const {backendUrl,companyToken} = useContext(AppContext);
 
@@ -64,7 +65,11 @@ const ManageJobs = () =>
   },[companyToken])
   // Whenever the companyToken changes it will execute the code inside useEffect.
 
-  return (
+  return jobs ? jobs.length === 0 ? (
+  <div className='flex items-center justify-center h-[70vh]'>
+    <p className='text-xl sm:text-2xl'>No Jobs Available or posted</p>
+  </div>
+) : (
     <div className='container p-4 max-w-5xl'>
       <div className='overflow-x-auto'>
         <table className='min-w-full bg-white border border-gray-200 max-sm:text-sm'>
@@ -98,7 +103,7 @@ const ManageJobs = () =>
       <button onClick={()=>navigate('/dashboard/add-job')} className='bg-black text-white py-2 px-4 rounded'>Add new job</button>
       </div>
     </div>
-  )
+  ) : <Loading/>
 }
 
 export default ManageJobs
--- client/src/pages/ViewApplications.jsx
+++ client/src/pages/ViewApplications.jsx
@@ -30,13 +30,40 @@ const ViewApplications = () =>
     }
   } 
 
+  // Function to update Job Application status
+  const changeJobApplicationStatus = async (id,status) => {
+      try{
+
+        const {data} = await axios.post(backendUrl+'/api/company/change-status',
+          {id,status},
+          {headers:{token:companyToken}}
+        )
+
+        if(data.success){
+          fetchCompanyJobApplications();
+        }
+
+        else{
+          toast.error(data.message);
+        }
+
+      }
+      catch(error){
+        toast.error(error.message);
+      }
+  }
+
   useEffect(() => {
     if(companyToken){
       fetchCompanyJobApplications();
     }
   },[companyToken])
 
-  return  applicants ? applicants.length === 0 ? (<div></div>) : (
+  return  applicants ? applicants.length === 0 ? (
+    <div className='flex items-center justify-center h-[70vh]'>
+    <p className='text-xl sm:text-2xl'>No Applications Available</p>
+  </div>
+) : (
     <div className='container max-auto p-4'>
         <div>
           <table className='w-full max-w-4xl bg-white border border-gray-200 max-sm:text-sm'>
@@ -54,7 +81,7 @@ const ViewApplications = () =>
               {applicants.filter(item => item.jobId && item.userId).map((applicant,index)=>(
                 <tr key={index} className='text-gray-700'>
                   <td className='py-2 px-4 border-b text-center'>{index+1}</td>
-                  <td className='py-2 px-4 border-b text-center flex'>
+                  <td className='py-2 px-4 border-b text-center flex items-center'>
                     <img className='w-10 h-10 rounded-full mr-3 max-sm:hidden' src={applicant.userId.image} alt="" />
                     <span>{applicant.userId.name}</span>
                   </td>
@@ -67,13 +94,22 @@ const ViewApplications = () =>
                   </a>
                   </td>
                   <td className='py-2 px-4 border-b relative'>
+                    
+                    {applicant.status === "Pending" 
+                    ?
                     <div className='relative inline-block text-left group'>
                       <button className='text-gray-500 action-button'>...</button>
                       <div className='z-10 hidden absolute right-0 md:left-0 top-0 mt-2 w-32 bg-white border border-gray-200 rounded shadow group-hover:block'>
-                        <button className='block w-full text-left px-4 py-2 text-blue-500 hover:bg-gray-100'>Accept</button>
-                        <button className='block w-full text-left px-4 py-2 text-red-500 hover:bg-gray-100'>Reject</button>
+                        <button onClick={()=>changeJobApplicationStatus(applicant._id,'Accepted')} className='block w-full text-left px-4 py-2 text-blue-500 hover:bg-gray-100'>Accept</button>
+                        <button onClick={()=>changeJobApplicationStatus(applicant._id,'Rejected')} className='block w-full text-left px-4 py-2 text-red-500 hover:bg-gray-100'>Reject</button>
                       </div>
                     </div>
+                    :  
+                    <div>
+                      {applicant.status}
+                    </div>
+                  }
+                    
                   </td>
                 </tr>
               ))}
